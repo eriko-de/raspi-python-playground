a8072ab5db07b57427eb425dc493284d568af2cb
eriko-de <erik.offenbach@outlook.de>
2022-06-12 20:57:46 +0200

    Add meta functions

--- gpio.py
+++ gpio.py
@@ -4,30 +4,29 @@ from gpiozero import Button
 import datetime
 import time
 
-def toRegie1A():
-    print('REGIE1A')
+def genConnectTo(to):
+    def wrapper():
+        print('Connect to ' + to)
+    return wrapper
 
-def toRegie1B():
-    print('REGIE1B')
-
-def toRegie2A():
-    print('REGIE2A')
-
-def toRegie2B():
-    print('REGIE2B')
-
+def genDisconnectFrom(from):
+    def wrapper():
+        print('Disconnect from ' + from)
+    return wrapper
 
 button1 = Button(5)
-button1.when_pressed = toRegie1A
+button1.when_pressed = genConnectTo("Regie1A")
+button1.when_released = genDisconnectFrom("Regie1A")
 
 button2 = Button(6)
-button2.when_pressed = toRegie1B
+button2.when_pressed = genConnectTo("Regie2A")
+button1.when_released = genDisconnectFrom("Regie2A")
 
-button3 = Button(13)
-button3.when_pressed = toRegie2A
-
-button4 = Button(19)
-button4.when_pressed = toRegie2B
+# button3 = Button(13)
+# button3.when_pressed = toRegie2A
+#
+# button4 = Button(19)
+# button4.when_pressed = toRegie2B
 
 while True:
     print('waiting....')
